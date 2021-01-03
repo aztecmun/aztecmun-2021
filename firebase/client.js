5d5da95117ebeb5f4e0d37ade62cbb46a8ff396e
Armando-Alamilla <armando_alamilla10@hotmail.com>
2021-01-02 19:19:59 -0600

    added firebase

---
+++ firebase/client.js
@@ -0,0 +1,35 @@
+import * as firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDFtaLUOYVBplfairoBowo9Fc0YRuHkr9c',
+  authDomain: 'aztecmun-2a3ec.firebaseapp.com',
+  projectId: 'aztecmun-2a3ec',
+  storageBucket: 'aztecmun-2a3ec.appspot.com',
+  messagingSenderId: '630465470366',
+  appId: '1:630465470366:web:a6f101362a3930d925408f',
+  measurementId: 'G-SPZCX8RR0X',
+}
+
+!firebase.apps.length && firebase.initializeApp(firebaseConfig)
+
+export const LoginWithEmail = (email, password) => {
+  return firebase.auth().signInWithEmailAndPassword(email, password)
+}
+
+export const createAccountWithEmail = (email, password, name) => {
+  return firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((result) => {
+      result.user.updatePropfile({
+        displayName: name,
+      })
+
+      const emailConfig = { url: 'https://localhost:3000' }
+
+      result.user.sendEmailVerification(emailConfig).catch((error) => {
+        console.error(error)
+      })
+    })
+}
