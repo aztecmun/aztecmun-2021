3d002e25d745a4e3ac390c3c76a5657d018c61e3
Dev-skybit <herrerabasurto@gmail.com>
2021-01-17 11:53:44 -0600

    Starting Test

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 // React import
 import React from 'react'
 
-import Layout from 'components/Layout'
 import GlobalStyles from 'styles/global'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </Layout>
+      <GlobalStyles />
+      <Component {...pageProps} />
     </>
   )
 }
