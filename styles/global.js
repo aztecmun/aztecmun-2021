32195d863ecd468c1f87b54e1d5773420d75e6f8
Dev-skybit <herrerabasurto@gmail.com>
2021-01-18 10:51:38 -0600

    test

--- styles/global.js
+++ styles/global.js
@@ -1,10 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-@font-face {
-    font-family: 'helvetica' ;
-    src: url('/HelveticaNeueBd.ttf') format('truetype');
-}   
 * {
     text-decoration: none;
     list-style: none;
