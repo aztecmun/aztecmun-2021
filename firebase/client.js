52b37d44ebc76b050971b10903e21dc36bd8ccf9
Armando-Alamilla <armando_alamilla10@hotmail.com>
2021-02-07 07:56:08 -0600

    Initial firebase config

--- firebase/client.js
+++ firebase/client.js
@@ -1,4 +1,7 @@
+// Firebase imports
 import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
