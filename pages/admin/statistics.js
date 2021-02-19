69d9532a51e286ca6706f26ca2e33f7017e52fb6
Armando-Alamilla <armando_alamilla10@hotmail.com>
2021-02-19 15:25:03 -0600

    statistics section added

--- pages/admin/statistics.js
+++ pages/admin/statistics.js
@@ -1,10 +1,10 @@
-// import React from 'react'
-// import { Bar } from 'react-chartjs-2'
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
 
-// export default function statistics() {
-//   return (
-//     <>
-//       <Bar data={} height={400} width={600} />
-//     </>
-//   )
-// }
+export default function statistics() {
+  return (
+    <>
+      <Bar data={{}} height={400} width={600} />
+    </>
+  )
+}
